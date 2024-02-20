d3f9618ef173871cc728fa1c409a502fa59feed3
Félix Hamel <4012997+felixhamel@users.noreply.github.com>
2024-02-20 00:42:15 -0500

    Update package.js
    Use latest version of Alanning Roles plugin.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'didericis:permissions-mixin',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'mdg:validated-method mixin for declaratively defining method permissions based on roles',
   git: 'https://github.com/didericis/permissions-mixin.git',
   documentation: 'README.md'
@@ -9,8 +9,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use(['ecmascript', 'check']);
-  api.use('alanning:roles@1.2.0');
-  api.use('mdg:validated-method@1.1.0');
+  api.use('alanning:roles@3.6.2');
+  api.use('mdg:validated-method@1.3.0');
   api.addFiles('permissions-mixin.js');
   api.export('PermissionsMixin');
 });
@@ -19,13 +19,12 @@ Package.onTest(function (api) {
     api.use([
       'ecmascript',
       'accounts-password',
-      'alanning:roles@1.2.0',
+      'alanning:roles@3.6.2',
       'practicalmeteor:mocha@2.1.0_5',
       'practicalmeteor:chai@2.1.0_1',
-      'aldeed:simple-schema@1.4.0',
-      'mdg:validated-method@1.0.2',
+      'mdg:validated-method@1.3.0',
       'mongo',
-      'didericis:permissions-mixin'
+      'didericis:permissions-mixin@1.1.3'
     ]);
 
     api.addFiles('permissions-mixin-tests.js');
